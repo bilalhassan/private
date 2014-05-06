fbc9284f0df89dda4fd98c9abbca8849b92f6240
bilalhassan <info@ottawapc.ca>
2014-05-05 20:42:57 -0400

    Update ar-theme.js

--- ar-theme.js
+++ ar-theme.js
@@ -9,46 +9,54 @@ jQuery(function($) {
 
     // honeycomb init
     $('.honeycombs').honeycombs();
+    // $('.tm-logo .fa').pulsate({color: '#ccc',onHover:false});
 
-    // homepage Salient
-    // $('.homepage-post').hover(function(){
-    // 	$('.homepage-post').not($(this)).stop(true,false).animate({'opacity':'0.9'},300);
-    // },function(){
-    // 	$('.homepage-post').stop(true,false).animate({'opacity':'1'},200);
-    // });
-    // gravity forms buttons
+    var last_position = 0;
     $('.gform_button').addClass('uk-button uk-button-primary');
 
+    $('.sidebar-post').effect();
     //side blog
     $('.tm-logo').click(function(){
         if($(this).hasClass('open')){
-            
             $(this).removeClass('open');
-            $('.uk-container,.tm-headerbar').animate({
+            $('.uk-container,.tm-headerbar').stop(true,false).animate({
                 'left':'350px'
             },200,function(){
                 $(this).animate({
                     'left':'0'
                 },500,function(){
-                    $('.side-blog').css({'background':'#ffffff','box-shadow':'none'});       
+                    // $('.side-blog').css({'box-shadow':'none'});       
                 });
-                // $('.side-blog').hide();
-                $('.sidebar-post').fadeOut(200);
+            });
+            $.ajax({
+                type: 'GET',
+                url: '/wp-content/themes/allroads/layouts/tracker.php?menu=0',
+                success: function(msg){
+                },
+                error: function(error){
+                }
             });     
         }else{
             $(this).addClass('open');
             // $('.side-blog').show();                    
-            $('.side-blog').css({'background':'#464646','background-image':'url("/wp-content/uploads/noise-bg.png")','background-repeat':'repeat','box-shadow':'-5px 0 5px #000 inset'});
-            $('.uk-container,.tm-headerbar').animate({
+            // $('.side-blog').css({'background':'#464646','box-shadow':'-5px 0 5px #000 inset'});
+            $('.uk-container,.tm-headerbar').stop(true,false).animate({
                 'left':'350px'
             },500,function(){
                 $(this).animate({
                     'left':'335px'
                 },200,function(){
-                    $('.sidebar-post').fadeIn('200');                                      
+                    // $('.sidebar-post').fadeIn('200');                                      
                 });
             }); 
-              
+            $.ajax({
+                type: 'GET',
+                url: '/wp-content/themes/allroads/layouts/tracker.php?menu=1',
+                success: function(msg){
+                },
+                error: function(error){
+                }
+            });               
         }
     });
 
@@ -65,8 +73,8 @@ jQuery(function($) {
             $('#sidebar-filter').animate({'right':'0'},475,function(){
                 $('img',this).css({'-webkit-transform':'rotate(0deg)','left':'-0'});
             });
-        }
-    })
+        };
+    });
     // menu
     $('.uk-navbar-nav li:first-child a,.uk-navbar-nav li:nth-child(2) a,.uk-navbar-nav li:nth-child(3) a').prepend('<span>Learn</span>');
 
@@ -84,6 +92,7 @@ jQuery(function($) {
         // alert(image + desc + url + name + position);
         $('#allroads-emp .pic').css({'background-image':'url(' + image + ')'});
         $('#allroads-emp h3').html(name);
+        $('#allroads-emp h4').html(position);
         $('#allroads-emp p').html(desc);
         $('#allroads-emp a').attr('href',url);
         $('#allroads-emp').fadeIn(300);
@@ -95,10 +104,9 @@ jQuery(function($) {
         $('#allroads-emp').hide();
         $('#allroads-emp .pic').css({'background-image':'url()'});
         $('#allroads-emp h3').html('');
+        $('#allroads-emp h4').html('');
         $('#allroads-emp p').html('');
         $('#allroads-emp a').attr('href','');
         // $('#allroads-emp').fadeIn(300);        
     }
-
-
 });
